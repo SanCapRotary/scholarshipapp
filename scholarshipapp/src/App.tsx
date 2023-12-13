f409729f5fbcbd4f8b8f24b563fd3e6798a8627b
Matthew Pierce <piercedev@outlook.com>
2023-12-13 00:02:26 -0500

    Add ContactUs component to test EmailJS system

--- scholarshipapp/src/App.tsx
+++ scholarshipapp/src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ContactUs } from './components/ContactUs';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,7 +29,11 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more...or just text Rick.
-      </p>
+          </p>
+          <div>
+              {/* Other components */}
+              <ContactUs />
+          </div>
     </>
   )
 }
---
+++ scholarshipapp/src/components/ContactUs.tsx
@@ -0,0 +1,68 @@
+import React, { useRef, useState, useEffect, FormEvent } from 'react';
+import emailjs from '@emailjs/browser';
+import './FormStyleSheet.css';
+
+export const ContactUs: React.FC = () => {
+    const form = useRef<HTMLFormElement>(null);
+    const [message, setMessage] = useState('');
+    const [wordCount, setWordCount] = useState(0);
+    const maxWords = 500;
+
+    useEffect(() => {
+        const words = message.trim().split(/\s+/);
+        const filteredWords = words.filter(word => word.length > 0);
+        setWordCount(filteredWords.length);
+    }, [message]);
+
+    const sendEmail = (e: FormEvent) => {
+        e.preventDefault();
+
+        if (form.current) {
+            emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+                .then((result) => {
+                    console.log(result.text);
+                }, (error) => {
+                    console.log(error.text);
+                });
+        }
+    };
+
+    const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const { value } = e.target;
+        // We count the words and update the state if they are less than or equal to maxWords
+        const words = value.trim().split(/\s+/);
+        if (words.length <= maxWords || value === "") {
+            setMessage(value);
+        }
+    };
+
+    return (
+        <form ref={form} onSubmit={sendEmail}>
+            <p>
+                <label>Name</label>
+                <input type="text" name="user_name" />
+            </p>
+
+            <p>
+                <label>Email</label>
+                <input type="email" name="user_email" />
+            </p>
+
+            <div className="textAreaContainer">
+                <label>Message</label>
+                <textarea
+                    name="message"
+                    value={message}
+                    onChange={handleMessageChange}
+                />
+                <div className="smallFont">
+                    Word Count: {wordCount}/{maxWords}
+                </div>
+            </div>
+
+            <p>
+                <input type="submit" value="Send" />
+            </p>
+        </form>
+    );
+};
