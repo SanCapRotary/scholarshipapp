4fc4e7c6e8c8bbaf172b6f87e6f6b47a8db4fd32
Matthew Pierce <piercedev@outlook.com>
2024-03-09 13:31:31 -0500

    Vite stupidity workaround

--- scholarshipapp/vite.config.ts
+++ scholarshipapp/vite.config.ts
@@ -1,8 +1,18 @@
+//import { defineConfig } from 'vite'
+//import react from '@vitejs/plugin-react'
+
+//// https://vitejs.dev/config/
+//export default defineConfig({
+//    base: "/scholarshipapp/",
+//  plugins: [react()],
+//})
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/scholarshipapp/",
-  plugins: [react()],
+    base: "/", // Serve from root
+    plugins: [react()],
 })
